607fe92f00aefab5356a24adc8ecf028be8f7aeb
Cathy Dao <cathydao95@gmail.com>
2022-07-19 11:04:00 -0700

    add comments

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import { deepClone, checkForWinner } from "../gameUtils";
 import GameInfo from "./GameInfo";
 
 function Main() {
+  // set board with 7 columns, 6 rows
   const [board, setBoard] = useState([
     [null, null, null, null, null, null],
     [null, null, null, null, null, null],
@@ -14,17 +15,20 @@ function Main() {
     [null, null, null, null, null, null],
   ]);
 
+  // set red/Player 1's turn first
   const [redTurn, setRedTurn] = useState(true);
   const [winner, setWinner] = useState("");
 
   console.log(board);
 
+  // when the board changes, check for a winner
   useEffect(() => {
     setWinner(window.connectFour.checkForWinner(board));
   }, [board]);
 
   console.log("winner", winner);
 
+  // function to reset states of board, winner, redTurn
   function restartGame() {
     setBoard([
       [null, null, null, null, null, null],
@@ -41,23 +45,27 @@ function Main() {
 
   return (
     <div>
+      {/* import component that displays game information */}
       <GameInfo redTurn={redTurn} winner={winner} />
+      {/* if there is a winner or draw, display the restart game button else, do not display anything */}
       {winner ? (
         <button className="restart-button" type="button" onClick={restartGame}>
           Restart Game
         </button>
       ) : null}
+      {/* map through each column on the board and then map through each cell in each column */}
       <div className="container">
         {board.map((column, index) => {
+          // create handleDrop function in the map to provide a function for each button in each column. Use indexOf to find the first index in which "null exists" and set as nextEmptySpot
           function handleDrop() {
             setRedTurn((prevTurn) => !prevTurn);
-            const nextEmptySpot = column.indexOf(null);
+            const nextEmpty = column.indexOf(null);
             setBoard((prevBoard) => {
               const clonedBoard = window.connectFour.deepClone(board);
               if (redTurn) {
-                clonedBoard[index][nextEmptySpot] = "Player 1";
+                clonedBoard[index][nextEmpty] = "Player 1";
               } else {
-                clonedBoard[index][nextEmptySpot] = "Player 2";
+                clonedBoard[index][nextEmpty] = "Player 2";
               }
               return clonedBoard;
             });
@@ -65,9 +73,11 @@ function Main() {
 
           return (
             <div key={index}>
+              {/* if there is a winner, do not display drop buttons */}
               {winner ? null : (
                 <Button
                   handleDrop={handleDrop}
+                  // disable button if a column is full or if there is a winner
                   disableButton={!column.includes(null) || winner}
                 />
               )}
